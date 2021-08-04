8d2f4d25d975416a5e72fb53cdc050c8ed3623af
HappyVasia <57608587+HappyVasia@users.noreply.github.com>
2021-08-04 12:29:12 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -8,7 +8,6 @@ $hostname="localhost";
          or die("Ошибка " . mysqli_error($link));
 	mysqli_set_charset($link,'utf8');
 
-
 $query = "SELECT * FROM книги";
 $result = $link->query($query);
 if (!$result) die($link->error); 
@@ -20,43 +19,5 @@ $row = $result->fetch_row(); //получение строки
 $knigi[] = $row;
  }
 echo json_encode($knigi);
-
-/*
-$query = "SELECT DISTINCT автор
-           FROM книги ORDER BY автор ";
-$result = $link->query($query);
-if (!$result) die($link->error); 
-$rows = $result->num_rows; 
-echo '<div class = "otbor">';
-echo '<div class = "otbor1"> АВТОРЫ 
-<i class="arrow down"></i>    ';
-echo '<div class = "otbor2">';
- for ($i = 0 ; $i < $rows ; ++$i){
-	$result->data_seek($i);  //выбор строки
-$row = $result->fetch_row(); //получение строки
-print<<< _HTML_
-
-*/
-
-
-/*
-$otbor = $_GET['otbor3'];
-$query = "SELECT картинка, книга_id, автор, название, 
-жанр, издательство, город_издания, год_издания, 
-стоимость_продажи
-           FROM книги WHERE автор = '$otbor' ";
-$result = $link->query($query);
-if (!$result) die($link->error); 
-$rows = $result->num_rows; 
-
-*/
-
-
-
-
-
-
-
-
 
 ?>
